b55a4e0518fa9fa5352f97e3d489a9877ee65597
Masud Rana <mr007masudrana@gmail.com>
2025-04-09 15:53:20 +0600

    type

---
+++ data-type.js
@@ -0,0 +1,34 @@
+// Check the data type of the variable's value
+
+const passedExam = true;
+const country = "Bangladesh";
+const price = 555;
+
+console.log(typeof passedExam);
+console.log(typeof country);
+console.log(typeof price);
+
+// Practice-1
+
+const lastPrice = 666;
+console.log( typeof lastPrice);
+
+// Practice-2
+const learning = true;
+console.log(typeof learning);
+
+// Practice-3
+const megaCity = "Dhaka";
+console.log(typeof megaCity);
+
+// Practice-4
+const temperature = 102.5;
+console.log(typeof temperature);
+
+// Practice-5
+const isHungry = true;
+console.log(typeof isHungry);
+
+// Practice-6
+const isRich = false;
+console.log(typeof isRich);
