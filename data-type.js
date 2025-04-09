a1de386dc744976407bc6ba12d6d6a2e1aa3ca15
Masud Rana <mr007masudrana@gmail.com>
2025-04-09 18:09:05 +0600

    comment

--- data-type.js
+++ data-type.js
@@ -95,5 +95,81 @@ console.log(chairsCount);
 // Practice-7
 let playTime = 3;
 playTime = 0;
-console.log(playTime);
+console.log(playTime); 
+
+
+
+// Use "comment" to write down any important information
+
+// one line commet 
+const userLevel = 4; //make user admin
+console.log(userLevel);
+
+// to off one line code 
+let price3 = 100;
+// price3 = 50;
+console.log(price3);
+
+// Many lines comment 
+
+// Total number of items in the shopping cart.
+// This value is initially set tp 0 because the user has not added any items yet.
+// It will increase wheneverthe user adds an item to chart.
+let totallitems = 0;
+
+// write multiple lines in a comment  
+
+/* 
+This variable stores the maximum number of attempts a user is allowed to make while logging into their account. it is set to 3. If the user exceeds this limit, their account will be temporarily locked.
+*/
+let maxLogAttempts = 3;
+
+// Practice-1
+
+//this is customers' feedback score
+let score = 70;
+
+
+// Practice-2
+
+// redius for heard circle
+// it is used to measure 
+// user may change it
+let radius = 20;
+
+// Practice-3
+
+/* there are 5 dresses: blue,whitw,yellow and red. these all are my favourite dress.
+I wear them at special programs.
+It is very cosy.
+*/
+let dress = 5;
+
+// Practice-4
+let giftMoney = 100; // this money has been given as a Eid Salami
+
+// Practice-5
+
+//bangla 1hour
+//english 2 hour
+//math 2 hour
+//islam 2 hour
+let studyTimes = 7;
+
+// Practice-6
+
+//he will get 1500 Tk 
+//Service charge that is 5 tk  will be cut from my balance  
+let sendMoney = 1500;
+
+// Practice-7
+
+//here product price is 600 tk and delivery fee is 55 tk.
+let pay = 655;
+
+
+
+
+
+
 
