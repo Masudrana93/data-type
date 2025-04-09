75450928dc739b19798f47264a41ce9712bed271
Masud Rana <mr007masudrana@gmail.com>
2025-04-09 16:41:34 +0600

    Let

--- data-type.js
+++ data-type.js
@@ -8,6 +8,12 @@ console.log(typeof passedExam);
 console.log(typeof country);
 console.log(typeof price);
 
+// Types of Variable
+// 1. Number:1, 2, 3, 4, 5 etc.
+// 2. String: "English", "Hello World"
+// 3. Boolean:true, false
+
+
 // Practice-1
 
 const lastPrice = 666;
@@ -32,3 +38,62 @@ console.log(typeof isHungry);
 // Practice-6
 const isRich = false;
 console.log(typeof isRich);
+
+//  Note: const means constant or fixed. so, you can't change any value of variables.
+
+
+// Use of "Let" to change or update the variables' values
+
+let priceT = 35;
+priceT = 45;
+console.log(priceT);
+
+let balance = 500;
+balance = 150;
+console.log(balance);
+
+
+// if you use 'const' instead of 'let', you can not change or update the value and output will show error. 
+
+// const name = "Kala Mia";
+// name = "Lal Mia";
+
+// console.log(name);
+
+
+// Practice-1 
+let age = 35;
+age = 34;
+console.log(age);
+
+// Practice-2
+let pocketMoney = 500;
+pocketMoney = 150;
+console.log(pocketMoney);
+
+// Practice-3
+let studyTime = 4;
+studyTime = 0;
+console.log(studyTime);
+
+// Practice-4
+let friendscount = 3;
+friendscount = 1;
+console.log(friendscount);
+
+// Practice-5
+let phonePrice = 25000;
+phonePrice = 12000;
+console.log(phonePrice);
+
+// Practice-6
+let chairsCount = 40;
+chairsCount = 38;
+chairsCount = 32;
+console.log(chairsCount);
+
+// Practice-7
+let playTime = 3;
+playTime = 0;
+console.log(playTime);
+
